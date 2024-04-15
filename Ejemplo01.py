58650415adfb41849857752bdf5ba0b19fe5c532
Salcedo86 <166466984+Salcedo86@users.noreply.github.com>
2024-04-14 23:44:50 -0500

    Tarea Modulo 1

---
+++ Ejemplo01.py
@@ -0,0 +1,7 @@
+# 1. Realizar un programa que pida tus datos personales para ingresar por teclado(inputs)
+#  e imprimirlos. 
+nombre = input("Hola Ingrese su nombre: ")
+apellidos = input("Ingresar sus apellidos: ")
+edad =int(input("¿Cuantos años tienes? "))
+
+print(f"Hola,{nombre} tus apellidos son {apellidos} y tienes {edad} años")
