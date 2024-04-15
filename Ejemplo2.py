8e3ca4f1fcba7b2a086feaf84563a627b0753154
Salcedo86 <166466984+Salcedo86@users.noreply.github.com>
2024-04-14 23:41:13 -0500

    Delete Ejemplo2.py

--- Ejemplo2.py
+++
@@ -1,8 +0,0 @@
-#nomenclatura de archivos python
-nombre = input('Ingrese su nombre: ')
-capital = float(input(f"{nombre} Ingrese su capital: "))
-tiempo = int(input("Ingresa la cantidad de años: "))
-
-capital_final = capital*(1+(5/100))**tiempo
-print(f'Hola {nombre} su capital final es {round(capital_final,2)}')
-print('fin')
